93f90272d040105631a3fd2c3fdc4acd72294333
alvin-git <2379243059@qq.com>
2016-03-28 19:24:50 +0800

    Initial commit 新建一个网站,建设中....

---
+++ src/java/com/viv/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.viv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by viv on 16-3-28.
+ * 该类在新建工程的时候用于测试工程是否建立成功
+ * http://localhost:8080/1.do
+ */
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/*", method = RequestMethod.GET)
+    public  String index() {
+        return "index.jsp";
+    }
+}
