5bae4cd70d44fc73cde3dfd511a8cd5f0feb9ee2
alvin-git <2379243059@qq.com>
2016-03-29 23:44:25 +0800

    (添加了.gitignore文件忽略.idea目录 同时移除了.idea目录,测试了返回json.)

---
+++ src/java/com/viv/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.viv.controller;
+
+/**
+ * Created by viv on 16-3-28.
+ */
+public class UserController {
+}
---
+++ src/java/com/viv/entity/User.java
@@ -0,0 +1,7 @@
+package com.viv.entity;
+
+/**
+ * Created by viv on 16-3-28.
+ */
+public class User {
+}
