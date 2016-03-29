89bad8e9e71b80ae9caec94ac9a648e74ac70c63
alvin-git <2379243059@qq.com>
2016-03-29 23:52:09 +0800

    新建了.gitignore文件忽略并移除了.idea  测试了回传json

--- src/java/com/viv/controller/UserController.java
+++ src/java/com/viv/controller/UserController.java
@@ -1,7 +1,36 @@
 package com.viv.controller;
 
+import com.viv.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by viv on 16-3-28.
  */
+@Controller
 public class UserController {
+
+    private static Map<String,User> users;
+
+    public UserController(){
+        if(users==null){
+            users =new HashMap<String,User>();
+        }
+        users.put("viv",new User("viv","man","GD",0111111,"11@11.com"));
+    }
+
+    @RequestMapping(value = "/list.do",params = "json")
+
+    public @ResponseBody Map<String,User> list(){
+        User u = new User();
+        u.setName("viv");
+        u.setHome("dd");
+        u.setSex("dd");
+        users.put("u",u);
+        return users;
+    }
 }
--- src/java/com/viv/entity/User.java
+++ src/java/com/viv/entity/User.java
@@ -4,4 +4,74 @@ package com.viv.entity;
  * Created by viv on 16-3-28.
  */
 public class User {
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", home='" + home + '\'' +
+                ", tel=" + tel +
+
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public User(){
+
+    }
+
+    public User(String name,String sex,String home, Integer tel,String email ){
+        this.name = name;
+        this.sex = sex;
+        this.home = home;
+        this.tel = tel;
+        this.email = email;
+    }
+
+    public String name;
+    public String sex;
+    public String home;
+    public Integer tel;
+    public String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public void setHome(String home) {
+        this.home = home;
+    }
+
+    public Integer getTel() {
+        return tel;
+    }
+
+    public void setTel(Integer tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
